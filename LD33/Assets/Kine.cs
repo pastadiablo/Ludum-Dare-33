ba18ae070ed4f86bffd33bcbd1275c99fd9029c6
John M. Will <john@johnmwill.com>
2015-08-22 15:36:18 -0700

    Added A* pathfinding to placeholder Kines (look like SpiderBots right now). Kines avoid alleys (purple), buildings (red), and reselect a new destination to path to once they arrive at their current one.

---
+++ LD33/Assets/Kine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class Kine : MonoBehaviour {
+
+	public MineBotAI AI;
+	public delegate void TargetReachedAction(Kine kine);
+	public event TargetReachedAction OnTargetReached;
+
+	void Awake () {
+		this.AI = GetComponent<MineBotAI>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.AI.OnTargetReachedEvent += TargetReached;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TargetReached() {
+		OnTargetReached(this);
+	}
+}
---
+++ LD33/Assets/KineManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KineManager : MonoBehaviour {
+
+	public int NumberOfKines;
+	protected IList Kines;
+
+	void Awake () {
+		this.Kines = FindObjectsOfType(typeof(Kine));
+	}
+
+	// Use this for initialization
+	void Start () {
+		foreach (Kine kine in Kines) {
+			GameObject kineTarget = new GameObject(kine.name + " TARGET");
+			kineTarget.transform.parent = this.transform;
+			kineTarget.transform.localPosition = new Vector3(Random.value * 49, 0, Random.value * 49);
+
+			kine.AI.target = kineTarget.transform;
+
+			kine.OnTargetReached += KineReachedTarget;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void KineReachedTarget(Kine kine){
+		kine.AI.target.localPosition = new Vector3 (Random.value * 49, 0, Random.value * 49);
+	}
+}
