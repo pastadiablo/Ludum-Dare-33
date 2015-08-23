db1188001bdac5f6cf67a826f62c7d83a128dcf6
John M. Will <john@johnmwill.com>
2015-08-22 17:01:46 -0700

    Added more buildings and alleys

---
+++ LD33/Assets/CameraLocker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLocker : MonoBehaviour {
+
+	Quaternion rotation;
+
+	// Use this for initialization
+	void Awake () {
+		rotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.rotation = rotation;
+	}
+}
--- LD33/Assets/KineManager.cs
+++ LD33/Assets/KineManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class KineManager : MonoBehaviour {
-
+	public Transform NormalKinePrefab;
 	public int NumberOfKines;
 	protected IList Kines;
 
@@ -12,10 +12,17 @@ public class KineManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Create new Kines to hit the editor-specified max #
+		/*for (int k=this.Kines.Count; k<=NumberOfKines; k++) {
+			GameObject kine = Instantiate(NormalKinePrefab, RandomGridLocation(), Quaternion.identity) as GameObject;
+			kine.name = "Kine " + k;
+		}*/
+
+		// Set them all up and kick their routines off
 		foreach (Kine kine in Kines) {
 			GameObject kineTarget = new GameObject(kine.name + " TARGET");
 			kineTarget.transform.parent = this.transform;
-			kineTarget.transform.localPosition = new Vector3(Random.value * 49, 0, Random.value * 49);
+			kineTarget.transform.localPosition = RandomGridLocation();
 
 			kine.AI.target = kineTarget.transform;
 
@@ -26,6 +33,10 @@ public class KineManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+		
+	Vector3 RandomGridLocation() {
+		return new Vector3 (Random.value * 48 + 1, 0, Random.value * 48 + 1);
 	}
 
 	public void KineReachedTarget(Kine kine){
