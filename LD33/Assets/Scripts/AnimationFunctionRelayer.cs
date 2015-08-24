f8e2853f499bc9f20065c335ea9a62a3ed8dd53c
Dan Magnusson <pastadiablo@gmail.com>
2015-08-23 20:54:59 -0700

    First-pass pouncing implemented.

---
+++ LD33/Assets/Scripts/AnimationFunctionRelayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationFunctionRelayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void EndPounce() {
+		GetComponentInParent<Vampire>().EndPounce();
+	}
+}
--- LD33/Assets/Scripts/Vampire.cs
+++ LD33/Assets/Scripts/Vampire.cs
@@ -1,66 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(SpriteRenderer))]
 public class Vampire : MonoBehaviour {
 
 	public enum VampireState {
 		IDLE,
 		WALKING,
-		RUNNING
+		RUNNING,
+		POUNCING
 	};
 
 	public Vector2 movement;
 
 	public float walkSpeed = 1.0f;
 	public float runSpeed = 2.0f;
+	public float pounceSpeed = 3.0f;
 	float moveSpeed;
 
 	public VampireState currentState;
-
 
 	Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		animator = GetComponent<Animator>();
+		animator = GetComponentInChildren<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		//If player holding down left-shift, set movement speed to run, otherwise, to walk
-		if(Input.GetKey(KeyCode.LeftShift)) {
-			moveSpeed = runSpeed;
-		} else {
-			moveSpeed = walkSpeed;
-		}
-		
-		movement = new Vector2(0.0f, 0.0f);
+		if(currentState != VampireState.POUNCING) {
 
-		if(Input.GetKey(KeyCode.W)) { //UP 
-			movement = new Vector2(movement.x, moveSpeed);
-			currentState = VampireState.WALKING;
-		}
-		else if(Input.GetKey(KeyCode.S)) { //DOWN
-			movement = new Vector2(movement.x, -moveSpeed);
-			currentState = VampireState.WALKING;
-		}
+			//If player holding down left-shift, set movement speed to run, otherwise, to walk
+			if(Input.GetKey(KeyCode.LeftShift)) {
+				moveSpeed = runSpeed;
+			} else {
+				moveSpeed = walkSpeed;
+			}
+
+			movement = new Vector2(0.0f, 0.0f);
+
+			if(Input.GetKey(KeyCode.W)) { //UP 
+				movement = new Vector2(movement.x, 1);
+				currentState = VampireState.WALKING;
+			}
+			else if(Input.GetKey(KeyCode.S)) { //DOWN
+				movement = new Vector2(movement.x, -1);
+				currentState = VampireState.WALKING;
+			}
+			
+			if(Input.GetKey(KeyCode.A)) { //LEFT
+				movement = new Vector2(-1, movement.y);
+				currentState = VampireState.WALKING;
+			}
+			else if(Input.GetKey(KeyCode.D)) { //RIGHT
+				movement = new Vector2(1, movement.y);
+				currentState = VampireState.WALKING;
+			}
 		
-		if(Input.GetKey(KeyCode.A)) { //LEFT
-			movement = new Vector2(-moveSpeed, movement.y);
-			currentState = VampireState.WALKING;
+			if (movement.x == 0.0f && movement.y == 0.0f ) {
+				currentState = VampireState.IDLE;
+			} else if (Input.GetKey (KeyCode.LeftShift)) {
+				currentState = VampireState.RUNNING;
+			}
 		}
-		else if(Input.GetKey(KeyCode.D)) { //RIGHT
-			movement = new Vector2(moveSpeed, movement.y);
-			currentState = VampireState.WALKING;
-		}
-	
-		if (movement.x == 0.0f && movement.y == 0.0f) {
-			currentState = VampireState.IDLE;
-		} else if (Input.GetKey (KeyCode.LeftShift)) {
-			currentState = VampireState.RUNNING;
+
+		if(currentState != VampireState.IDLE) {
+			if(Input.GetKeyDown(KeyCode.Space)) {
+				currentState = VampireState.POUNCING;
+			}
 		}
 
 		switch(currentState) {
@@ -77,13 +85,26 @@ public class Vampire : MonoBehaviour {
 			animator.SetBool("moving", true);
 
 			break;
+		case VampireState.POUNCING:
+
+			animator.SetBool("pouncing", true);
+			animator.SetBool("moving", false);
+			moveSpeed = pounceSpeed;
+
+			break;
 		}
+
 
 		animator.SetInteger("horizontal", (int)movement.x);
 		animator.SetInteger("vertical", (int)movement.y);
 
-		transform.localPosition = new Vector3(transform.localPosition.x + movement.x * Time.deltaTime,
-		                                      transform.localPosition.y + movement.y * Time.deltaTime,
+		transform.localPosition = new Vector3(transform.localPosition.x + movement.x * Time.deltaTime * moveSpeed,
+		                                      transform.localPosition.y + movement.y * Time.deltaTime * moveSpeed,
 		                                      0);
+	}
+
+	public void EndPounce() {
+		animator.SetBool("pouncing", false);
+		currentState = VampireState.IDLE;
 	}
 }
